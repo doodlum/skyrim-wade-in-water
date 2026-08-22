afab5ce08bebb8fb764a83e5cdaee0228f270b7f
doodlum <15017472+doodlum@users.noreply.github.com>
2026-08-22 21:34:25 +0100

    Fix SKSE AE structure compatibility metadata

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -55,7 +55,7 @@ EXTERN_C [[maybe_unused]] __declspec(dllexport) constinit auto SKSEPlugin_Versio
 	v.PluginName(Plugin::NAME.data());
 	v.PluginVersion(Plugin::VERSION);
 	v.UsesAddressLibrary();
-	v.UsesNoStructs();
+	v.UsesUpdatedStructs();
 	return v;
 }();
 
