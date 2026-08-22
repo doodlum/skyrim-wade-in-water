d1da317514ee79d1e286c54c0a32fa6b7b8c1324
doodlum <15017472+doodlum@users.noreply.github.com>
2026-08-22 18:16:34 +0100

    Add Skyrim 1.7.99 support with CommonLibSSE-NG

--- cmake/XSEPlugin.cmake
+++ cmake/XSEPlugin.cmake
@@ -1,3 +1,10 @@
+set(CommonLibPath "${CMAKE_SOURCE_DIR}/extern/CommonLibSSE-NG" CACHE PATH "Path to CommonLibSSE-NG")
+set(COMMONLIB_PREBUILT ON CACHE BOOL "Use prebuilt CommonLibSSE-NG" FORCE)
+set(COMMONLIB_PREBUILT_MULTICONFIG ON CACHE BOOL "Use prebuilt CommonLibSSE-NG with multi-config generators" FORCE)
+set(ENABLE_SKYRIM_SE ON CACHE BOOL "Enable Skyrim SE support" FORCE)
+set(ENABLE_SKYRIM_AE ON CACHE BOOL "Enable Skyrim AE support" FORCE)
+set(ENABLE_SKYRIM_VR ON CACHE BOOL "Enable Skyrim VR support" FORCE)
+
 option(BUILD_SKYRIM "Build for Skyrim" OFF)
 option(BUILD_SKYRIMVR "Build for Skyrim VR" OFF)
 option(BUILD_FALLOUT4 "Build for Fallout 4" OFF)
@@ -91,7 +98,6 @@ if (CMAKE_GENERATOR MATCHES "Visual Studio")
 		"${PROJECT_NAME}"
 		PRIVATE
 			/MP
-			/await
 			/W4
 			/WX
 			/permissive-
@@ -136,7 +142,7 @@ find_package(nlohmann_json CONFIG REQUIRED)
 find_package(magic_enum CONFIG REQUIRED)
 
 if (BUILD_SKYRIM)
-	find_package(CommonLibSSE REQUIRED)
+	add_subdirectory(${CommonLibPath} ${CommonLibName} EXCLUDE_FROM_ALL)
 	target_link_libraries(
 		${PROJECT_NAME} 
 		PUBLIC 
